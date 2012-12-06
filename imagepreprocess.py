ca609cc02c26be3df3517157993d64efad74320a
bksim <simpavid@gmail.com>
2012-12-06 02:16:52 -0500

    basic preprocessing algorithm

---
+++ imagepreprocess.py
@@ -0,0 +1,34 @@
+import Image
+import os, sys
+
+def save_JPEG_sequence(filename):
+	im = Image.open(filename)
+	counter = 0
+	try:
+	    while True:
+	    	im = Image.open(filename)
+
+	    	while im.tell() < counter:
+	        	im.seek(im.tell()+1)
+
+	        if im.mode != "RGB":
+	        	im = im.convert("RGB")
+
+	        # saves files in sequenceDir
+	        os.chdir("sequenceDir")
+	        im.save("t2_axial" + str(counter) + ".jpg", "JPEG")
+	        os.chdir("..")
+	        # returns back to original directory
+
+	        print "t2_axial" + str(counter)
+	        counter+=1
+	except EOFError:
+	    pass # end of sequence
+
+	im.close()
+	return
+
+# main
+if __name__ == "__main__":
+	save_JPEG_sequence("t2_axial.gif")
+
