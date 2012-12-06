b4909731a6a11f018956bf5f6a2f5a2e2ebe7221
Wesley Chen <wesleychen@college.harvard.edu>
2012-12-06 15:09:29 -0500

    baghdad full with transparency overlay at end

--- baghdad_wc.m
+++ baghdad_wc.m
@@ -66,13 +66,16 @@ I3(fgm4) = 255;
 figure, imshow(I3)
 title('Modified regional maxima superimposed on original image (fgm4)')
 
-%bw = im2bw(Iobrcbr, graythresh(Iobrcbr));
-%figure, imshow(bw), title('Thresholded opening-closing by reconstruction (bw)')
 
-%D = bwdist(bw);
-%DL = watershed(D);
-%bgm = DL == 0;
-%figure, imshow(bgm), title('Watershed ridge lines (bgm)')
+%this next step should be unnecessary as no need to mark already black
+%background
+bw = im2bw(Iobrcbr, graythresh(Iobrcbr));
+figure, imshow(bw), title('Thresholded opening-closing by reconstruction (bw)')
+
+D = bwdist(bw);
+DL = watershed(D);
+bgm = DL == 0;
+figure, imshow(bgm), title('Watershed ridge lines (bgm)')
 
 gradmag2 = imimposemin(gradmag, bgm | fgm4);
 L = watershed(gradmag2);
@@ -85,4 +88,7 @@ title('Markers and object boundaries superimposed on original image (I4)')
 Lrgb = label2rgb(L, 'jet', 'w', 'shuffle');
 figure, imshow(Lrgb)
 title('Colored watershed label matrix (Lrgb)')
- 
+hold on;
+handle = imshow(I);
+alpha(0.5);
+hold off;
