3566cfa1368098926e5e3ff97ec2f3caab3bbedd
Wesley Chen <wesleychen@college.harvard.edu>
2012-12-11 00:45:30 -0500

    added grayscale only just for MRI image

--- forMRI_wc.m
+++ forMRI_wc.m
@@ -1,10 +1,7 @@
-%baghdad
-%edge + overlayed watershed transform = result
+%using watershed segmentation on the MRI image
 
 %read image
-I = imread('lilypad.jpg');
-
-I = rgb2gray(I);
+I = imread('1.jpg');
 
 %using default threshold for now
 BW = edge(I,'sobel');
@@ -28,56 +25,57 @@ Iy = imfilter(double(I), hy, 'replicate');
 Ix = imfilter(double(I), hx, 'replicate');
 
 gradmag = sqrt(Ix.^2 + Iy.^2);
-figure, imshow(gradmag,[]), title ('Gradient Magnitude')
+%figure, imshow(gradmag,[]), title ('Gradient Magnitude')
 
 L = watershed(gradmag);
 Lrgb = label2rgb(L);
-figure, imshow(Lrgb, []), title('Watershed Transform of Gradient - oversegmentation')
+%figure, imshow(Lrgb, []), title('Watershed Transform of Gradient - oversegmentation')
 
-se = strel('disk', 3);
+%use 2-5 5 makes the tumor fade away completely
+se = strel('disk', 4);
 Io = imopen(I, se);
-figure, imshow(Io), title('Opening (Io)')
+%figure, imshow(Io), title('Opening (Io)')
 
 Ie = imerode(I, se);
 Iobr = imreconstruct(Ie, I);
-figure, imshow(Iobr), title('Opening-by-reconstruction (Iobr)')
+%figure, imshow(Iobr), title('Opening-by-reconstruction (Iobr)')
 
 Ioc = imclose(Io, se);
-figure, imshow(Ioc), title('Opening-closing (Ioc)')
-%{
+%figure, imshow(Ioc), title('Opening-closing (Ioc)')
+
 Iobrd = imdilate(Iobr, se);
 Iobrcbr = imreconstruct(imcomplement(Iobrd), imcomplement(Iobr));
 Iobrcbr = imcomplement(Iobrcbr);
-figure, imshow(Iobrcbr), title('Opening-closing by reconstruction (Iobrcbr)')
+%figure, imshow(Iobrcbr), title('Opening-closing by reconstruction (Iobrcbr)')
 
 fgm = imregionalmax(Iobrcbr);
-figure, imshow(fgm), title('Regional maxima of opening-closing by reconstruction (fgm)')
+%figure, imshow(fgm), title('Regional maxima of opening-closing by reconstruction (fgm)')
 
 I2 = I;
 I2(fgm) = 255;
-figure, imshow(I2), title('Regional maxima superimposed on original image (I2)')
+%figure, imshow(I2), title('Regional maxima superimposed on original image (I2)')
 
 se2 = strel(ones(3,3));
 fgm2 = imclose(fgm, se2);
 fgm3 = imerode(fgm2, se2);
-figure, imshow(I2), title('Regional maxima superimposed on original image after Erosion (I2)')
+%figure, imshow(I2), title('Regional maxima superimposed on original image after Erosion (I2)')
 
 fgm4 = bwareaopen(fgm3, 20);
 I3 = I;
 I3(fgm4) = 255;
-figure, imshow(I3)
+%figure, imshow(I3)
 title('Modified regional maxima superimposed on original image (fgm4)')
 
 
 %this next step should be unnecessary as no need to mark already black
 %background
 bw = im2bw(Iobrcbr, graythresh(Iobrcbr));
-figure, imshow(bw), title('Thresholded opening-closing by reconstruction (bw)')
+%figure, imshow(bw), title('Thresholded opening-closing by reconstruction (bw)')
 
 D = bwdist(bw);
 DL = watershed(D);
 bgm = DL == 0;
-figure, imshow(bgm), title('Watershed ridge lines (bgm)')
+%figure, imshow(bgm), title('Watershed ridge lines (bgm)')
 
 gradmag2 = imimposemin(gradmag, bgm | fgm4);
 L = watershed(gradmag2);
@@ -90,8 +88,10 @@ title('Markers and object boundaries superimposed on original image (I4)')
 Lrgb = label2rgb(L, 'jet', 'w', 'shuffle');
 figure, imshow(Lrgb)
 title('Colored watershed label matrix (Lrgb)')
+
+figure, imshow(Lrgb)
+title('Colored watershed label matrix (Lrgb) superimposed')
 hold on;
 handle = imshow(I);
 alpha(0.5);
 hold off;
-%}
